3960e396a0d3f8f9157adca12980906f7ff6ca4b
jovannil94 <jovanniluna@pursuit.org>
2020-07-23 11:08:25 -0400

    adds select to Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,37 +2,36 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../css/Navbar.css";
 // import logo from "../images/Reddit-Logo-Horizontal.png";
-// import { apiURL } from "../utilities/apiURL";
-// import axios from "axios";
+import axios from "axios";
 // import SubredditIndex from "../helpers/SubredditIndex";
 
 const NavBar = () => {
     const [subreddits, setSubreddits] = useState([]);
-    // const API = apiURL();
     
-    // const fetchSubreddits = async () => {
-    //     try {
-    //         let res = await axios({
-    //             method: "get",
-    //             url: `${API}/subreddits/`
-    //         })
-    //         setSubreddits(res.data.payload)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
+    const fetchSubreddits = async () => {
+        try {
+            let res = await axios.get(`http://localhost:3001/subreddits/`)
+            setSubreddits(res.data.payload)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     useEffect(() => {
-        // fetchSubreddits()
+        fetchSubreddits()
     }, []);
 
     return(
         <nav className="Navbar">
             {/* <img src={logo} className="Logo" alt="" />
                 <SubredditIndex subreddits={subreddits}/> */}
-            {/* <select>
-                <option value=""  hidden>Home</option>
-            </select> */}
+            <select>
+                <option value="">Home</option>
+                {subreddits.map((subreddit) => 
+                    <option key={subreddit.id} value={subreddit.id}>{subreddit.subname}</option>
+                    //need to create an on change function to redirect to that specific sub page
+                )}
+            </select>
             <NavLink className="Links" exact to={"/"}>Home</NavLink>
             <NavLink className="Links" to={"/login"}>Log In</NavLink>
         </nav>
