53148c25c79d2cf4ae20d86e1cccac1e09a59a87
jovannil94 <jovanniluna@pursuit.org>
2020-08-31 21:56:54 -0400

    fixing search bar in Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,8 +4,11 @@ import "../css/Navbar.css";
 import logo from "../images/redditLogo.png";
 import axios from "axios";
 
+
 const NavBar = () => {
     const [subreddits, setSubreddits] = useState([]);
+    const [display, setDisplay] = useState(false);
+    const [search, setSearch] = useState("");
     const history = useHistory();
     const subredditRedirect = (selected) => history.push(`/subreddit/${selected}`);
     const homeRedirect = () => history.push(`/`);
@@ -19,6 +22,10 @@ const NavBar = () => {
         }
     }
 
+    const handleLogoClick = () => {
+        homeRedirect()
+    }
+
     const handleChange = (e) => {
         e.preventDefault();
         if(e.currentTarget.value === "Home") {
@@ -29,20 +36,45 @@ const NavBar = () => {
         }
     }
 
+    const handleSearchClick = (e) => {
+        e.preventDefault();
+        subredditRedirect(e.currentTarget.textContent)
+    }
+
+    const setSubFilter = subreddit => {
+        setSearch(subreddit);
+        setDisplay(false);
+    }
+
     useEffect(() => {
         fetchSubreddits()
     }, []);
 
     return(
         <nav className="Navbar">
-            <img src={logo} className="Logo" alt="" />
+            <img src={logo} className="Logo" alt="" onClick={handleLogoClick}/>
                 {/* <SubredditIndex subreddits={subreddits}/> */}
-            <select onChange={handleChange}>
+            <select className="NavbarSelect" onChange={handleChange}>
                 <option value="Home">Home</option>
                 {subreddits.map((subreddit) => 
                     <option key={subreddit.id} value={ subreddit.id, subreddit.name }>/r{subreddit.subname}</option>
                 )}
             </select>
+
+            <input type="text" placeholder="Search" onClick={() => setDisplay(!display)} onChange={(e) => setSearch(e.target.value)} value={search}/>
+            {display && (
+                <div className="SearchContainer">
+                    {subreddits.filter(({subname}) => subname.indexOf(search.toLowerCase()) >= 0).map((sub, i) => {
+                        return(
+                            <div className="SearchResults" key={i} onClick={() => setSubFilter(sub.subname)} value={sub.subname}>
+                                <span>
+                                    {sub.subname}
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
             <NavLink className="Links" exact to={"/"}>Home</NavLink>
             <NavLink className="Links" to={"/login"}>Log In</NavLink>
         </nav>
