7a9491f3e835e980ec35f94b4a17215969dd7bea
jovannil94 <jovanniluna@pursuit.org>
2021-02-25 19:14:28 -0500

    adding to subreddit subscribe option

--- frontend/src/components/Subreddit.js
+++ frontend/src/components/Subreddit.js
@@ -1,28 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import DisplayPost from "../helper/DisplayPosts";
+import { UserContext } from "../provider/UserProvider";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import "../css/Subreddit.css"
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+      borderRadius: "5em"
+    },
+  }));
 
 const Subreddit = () => {
     const [getDetails, setGetDetails] = useState([]);
     const { id } = useParams();
-
+    const { userID } = useContext(UserContext);
+    const [subscribed, setSubscribed] = useState(false);
+    const classes = useStyles();
+    
     useEffect(() => {
+        const isUserSubscribed = async () => {
+            try {
+                let res = await axios.get(`http://localhost:3001/subscriptions/usersubbed/${userID}/${id}`);
+                debugger
+                if(res.data.payload.length > 0) {
+                    setSubscribed(true)
+                    //payload is returning with an empty array also userID not showing up, edit id for sub
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
         const fetchDetails = async () => {
-        try {
-            let res = await axios.get(`http://localhost:3001/subreddits/${id}`);
-            setGetDetails(res.data.payload);
-        } catch (error) {
-            console.log(error)
-        }
-    }
+            try {
+                let res = await axios.get(`http://localhost:3001/subreddits/${id}`);
+                setGetDetails(res.data.payload);
+            } catch (error) {
+                console.log(error)
+            }
+        }; 
         fetchDetails();
+        // isUserSubscribed();
     }, [id])
 
     return (
         <div className="subContainer">
-            <h1 className="subTitle">/r/{getDetails.subname}</h1>
+            <div className="subInfo">
+                <div className="subHeader">
+                    <h1 className="subTitle">{getDetails.subname}</h1>
+                    <p className="subRoute">/r/{getDetails.subname}</p>
+                </div>
+                { subscribed ? 
+                <Button className={classes.root} variant="contained" color='secondary' type="submit" size={'small'}>Leave</Button>
+                :<Button className={classes.root} variant="contained" color='secondary' type="submit" size={'small'}>Join</Button>
+                }
+            </div>
             <div className="subFeed">
                 <DisplayPost choosen={getDetails.id}/>
             </div>
